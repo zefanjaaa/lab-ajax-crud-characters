8c22458dade7b80c4822ce9f845d93a4a33b3431
artweise <art2wsxweise7ujm@gmail.com>
2023-01-19 11:32:50 +0200

    iteration 3.1 done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,15 +1,54 @@
-const axios = require("axios");
-
 class APIHandler {
-  constructor() {
+  /*   constructor() {
     this.api = axios.create({
       baseURL: "localhost:8000",
     });
+  } */
+
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl;
+    this.api = axios.create({ baseURL: this.BASE_URL });
   }
 
-  getFullList() {
-    return this.api.get("/characters");
-  }
+  getFullList = () => {
+    const allCharactersView = document.querySelector(".characters-container");
+
+    return (
+      this.api
+        .get("/characters")
+        // Iteration 3.1
+        .then((result) => {
+          console.log(result.data);
+          const charactersList = result.data;
+          allCharactersView.innerHTML = "";
+          // approach with for loop
+          /* for (let i = 0; i < charactersList.length; i++) {
+          allCharactersView.innerHTML += `<div class="character-info">
+    <div class="id">Id: ${charactersList[i].id}</div>
+    <div class="name">Name: ${charactersList[i].name}</div>
+    <div class="occupation">Occupation: ${charactersList[i].occupation}</div>
+    <div class="cartoon">Is a Cartoon? ${charactersList[i].cartoon}</div>
+    <div class="weapon">Weapon: ${charactersList[i].weapon}</div>
+  </div>`;
+        } */
+          // another approach with forEach()
+          charactersList.forEach((element) => {
+            const character = `<div class="character-info">
+              <div class="id">Id: ${element.id}</div>
+              <div class="name">Name: ${element.name}</div>
+              <div class="occupation">Occupation: ${element.occupation}</div>
+              <div class="cartoon">Is a Cartoon? ${element.cartoon}</div>
+              <div class="weapon">Weapon: ${element.weapon}</div>
+            </div>`;
+            allCharactersView.innerHTML += character;
+          });
+        })
+        .catch((error) => {
+          console.log(`Something went wrong getting all characters ${error}`);
+          allCharactersView.innerHTML = `<h3>Something went wrong. Try again later</h3>`;
+        })
+    );
+  };
 
   getOneRegister(characterid) {
     return this.api.get(`/characters/${characterid}`);
@@ -27,5 +66,3 @@ class APIHandler {
     return this.api.delete(`/characters/${characterid}`);
   }
 }
-
-module.exports = APIHandler;
